966a474c23e3b83811089019405174dcd29c3f22
Abhinav-Ar <88677626+Abhinav-Ar@users.noreply.github.com>
2026-06-04 18:40:34 -0700

    chud chud chud

--- auth.ts
+++ auth.ts
@@ -8,7 +8,12 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   adapter: PostgresAdapter(pool),
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
   callbacks: {
     session({ session, user }) {
       // Expose user.id on the session so API routes can use it
